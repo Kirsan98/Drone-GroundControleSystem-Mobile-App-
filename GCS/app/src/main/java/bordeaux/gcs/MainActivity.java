a3042fdf48ca81d121ed6dfb978b4d3f18c6b05b
Anthony Regnies <aregnies@emi.u-bordeaux1.fr>
2020-02-21 15:54:17 +0100

    correction sans google

--- GCS/app/src/main/java/bordeaux/gcs/MainActivity.java
+++ GCS/app/src/main/java/bordeaux/gcs/MainActivity.java
@@ -10,11 +10,6 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
